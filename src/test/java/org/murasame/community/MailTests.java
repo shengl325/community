433669fa037cfe1a7dd8592e5d3bccaeedefde0f
shengl325 <huoying74ls@gmail.com>
2019-12-19 01:05:26 +1100

    add the util for sending email

---
+++ src/test/java/org/murasame/community/MailTests.java
@@ -0,0 +1,37 @@
+package org.murasame.community;
+
+import org.junit.jupiter.api.Test;
+import org.murasame.community.util.MailClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() {
+        mailClient.sendMail("shengl6@student.unimelb.edu.au", "TEST", "Welcome.");
+    }
+
+    @Test
+    public void testHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("shengl6@student.unimelb.edu.au", "HTML", content);
+    }
+
+}
