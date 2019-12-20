11717fcea1e772486a41052a9104c0ac767d9586
shengl325 <huoying74ls@gmail.com>
2019-12-20 15:40:07 +1100

    add the login ticket interceptor

---
+++ src/main/java/org/murasame/community/util/CookieUtil.java
@@ -0,0 +1,27 @@
+package org.murasame.community.util;
+
+import org.springframework.web.bind.annotation.CookieValue;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CookieUtil {
+
+    public static String getValue(HttpServletRequest request, String name) {
+        if (request == null || name == null) {
+            throw new IllegalArgumentException("参数为空!");
+        }
+
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(name)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+
+}
