904f38c75f3a0e043004ad18639aa74a10e25f01
shengl325 <huoying74ls@gmail.com>
2019-12-20 22:35:12 +1100

    add the function: view post details

--- src/main/java/org/murasame/community/controller/DiscussPostController.java
+++ src/main/java/org/murasame/community/controller/DiscussPostController.java
@@ -3,10 +3,13 @@ package org.murasame.community.controller;
 import org.murasame.community.entity.DiscussPost;
 import org.murasame.community.entity.User;
 import org.murasame.community.service.DiscussPostService;
+import org.murasame.community.service.UserService;
 import org.murasame.community.util.CommunityUtil;
 import org.murasame.community.util.HostHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +22,9 @@ public class DiscussPostController {
 
     @Autowired
     private DiscussPostService discussPostService;
+
+    @Autowired
+    private UserService userService;
 
     @Autowired
     private HostHolder hostHolder;
@@ -39,6 +45,18 @@ public class DiscussPostController {
         discussPostService.addDiscussPost(post);
 
         return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+    @RequestMapping(path = "/detail/{discussPostId}", method =  RequestMethod.GET)
+    public String getDiscussPost(@PathVariable("discussPostId") int discussPostId, Model model) {
+        // 帖子
+        DiscussPost post = discussPostService.findDiscussPostById(discussPostId);
+        model.addAttribute("post", post);
+        // 作者
+        User user = userService.findUserById(post.getUserId());
+        model.addAttribute("user", user);
+
+        return "/site/discuss-detail";
     }
 
 }
