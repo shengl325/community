e8c0f7c0293fc76b493e3b511c5f77ee7040d914
shengl325 <huoying74ls@gmail.com>
2019-12-23 16:14:35 +1100

    handle controller exceptions

--- src/main/java/org/murasame/community/controller/HomeController.java
+++ src/main/java/org/murasame/community/controller/HomeController.java
@@ -32,7 +32,6 @@ public class HomeController {
         page.setRows(discussPostService.findDiscussPostRows(0));
         page.setPath("/index");
 
-
         List<DiscussPost> list = discussPostService.findDiscussPosts(0, page.getOffset(), page.getLimit());
         List<Map<String, Object>> discussPosts = new ArrayList<>();
         if (list != null) {
@@ -46,6 +45,11 @@ public class HomeController {
         }
         model.addAttribute("discussPosts", discussPosts);
         return "/index";
+    }
+
+    @RequestMapping(path = "/error", method = RequestMethod.GET)
+    public String getErrorPage() {
+        return "/error/500";
     }
 
 }
