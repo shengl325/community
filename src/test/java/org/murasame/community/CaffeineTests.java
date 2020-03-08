ab7b99f966b4497c4015cf818cc6c152a5f61d6c
shengl325 <huoying74ls@gmail.com>
2020-03-08 17:59:46 +0800

    update README.md.

---
+++ src/test/java/org/murasame/community/CaffeineTests.java
@@ -0,0 +1,39 @@
+package org.murasame.community;
+
+import org.junit.jupiter.api.Test;
+import org.murasame.community.entity.DiscussPost;
+import org.murasame.community.service.DiscussPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Date;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class CaffeineTests {
+
+    @Autowired
+    private DiscussPostService postService;
+
+    @Test
+    public void initDataForTest() {
+        for (int i = 0; i < 300000; i++) {
+            DiscussPost post = new DiscussPost();
+            post.setUserId(111);
+            post.setTitle("压力测试");
+            post.setContent("测试测试测试测试测试测试测试测试测试测试测试测试");
+            post.setCreateTime(new Date());
+            post.setScore(Math.random() * 2000);
+            postService.addDiscussPost(post);
+        }
+    }
+
+    @Test
+    public void testCache() {
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 1));
+        System.out.println(postService.findDiscussPosts(0, 0, 10, 0));
+    }
+
+}
